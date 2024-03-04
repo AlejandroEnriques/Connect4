5f9360062436142e6fab6aa9641c88072581efcf
AlejandroEnriques <46725394+AlejandroEnriques@users.noreply.github.com>
2024-03-04 12:48:13 -0600

    Clean the terminal when you start

--- Screen.py
+++ Screen.py
@@ -1,9 +1,15 @@
 import importlib
 import Game
+import os
+
+
 importlib.reload(Game)
 
 # Main Menu
+def clear_terminal():
+    os.system('cls')
 
+clear_terminal()
 print('''Lets play Connect 4!
 You need 2 players to play!
 Player 1 = X
